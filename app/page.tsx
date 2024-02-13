dbf3a67a0efa8a44bb6e9febf2fdea8e2e652c50
David Zheng <hello@davidzheng.me>
2024-02-13 14:51:06 -0800

    disable globe for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { AnimatePresence, LazyMotion, domAnimation, m } from "framer-motion"
+// import Globe from "react-globe.gl"
 import createGlobe from "cobe"
 import { useRef, useEffect } from "react"
 export default function Home() {
@@ -8,68 +9,72 @@ export default function Home() {
   const globeState = useRef({ phi: 0, theta: 0 })
   const focusRef = useRef([0, 0])
 
-  useEffect(() => {
-    let width = 0
-    const onResize = () =>
-      canvasRef.current && (width = canvasRef.current.offsetWidth)
-    window.addEventListener("resize", onResize)
-    onResize()
-    const globe = createGlobe(canvasRef.current!, {
-      devicePixelRatio: 2,
-      width: width * 2,
-      height: width * 2,
-      phi: 0,
-      theta: 0.3,
-      dark: 1,
-      diffuse: 3,
-      mapSamples: 12000,
-      mapBrightness: 1.2,
-      baseColor: [1, 1, 1],
-      markerColor: [0 / 255, 30 / 255, 120 / 255],
-      glowColor: [1.2, 1.2, 1.2],
-      markers: [
-        { location: [49.246292, -123.116226], size: 0.05 },
-        // { location: [114.177216, 22.302711], size: 0.05 },
-        // { location: [-118.243683, 34.052235], size: 0.05 },
-        // { location: [-104.991531, 39.742043], size: 0.05 },
-        // { location: [103.851959, 1.290270], size: 0.05 },
-        // { location: [-74.0060, 40.7128], size: 0.05 }
-      ],
-      onRender: (state) => {
-        const { current } = globeState
+  // useEffect(() => {
+  //   let width = 0
+  //   let globe
+  //   const onResize = () =>
+  //     canvasRef.current && (width = canvasRef.current.offsetWidth)
+  //   window.addEventListener("resize", onResize)
+  //   onResize()
+  //   if (canvasRef.current) {
+  //     globe = createGlobe(canvasRef?.current, {
+  //       devicePixelRatio: 2,
+  //       width: width * 2,
+  //       height: width * 2,
+  //       phi: 0,
+  //       theta: 0.3,
+  //       dark: 1,
+  //       diffuse: 3,
+  //       mapSamples: 12000,
+  //       mapBrightness: 1.2,
+  //       baseColor: [1, 1, 1],
+  //       markerColor: [0 / 255, 30 / 255, 120 / 255],
+  //       glowColor: [1.2, 1.2, 1.2],
+  //       markers: [
+  //         { location: [49.246292, -123.116226], size: 0.05 },
+  //         // { location: [114.177216, 22.302711], size: 0.05 },
+  //         // { location: [-118.243683, 34.052235], size: 0.05 },
+  //         // { location: [-104.991531, 39.742043], size: 0.05 },
+  //         // { location: [103.851959, 1.290270], size: 0.05 },
+  //         // { location: [-74.0060, 40.7128], size: 0.05 }
+  //       ],
+  //       onRender: (state) => {
+  //         const { current } = globeState
 
-        const [focusPhi, focusTheta] = focusRef.current
+  //         const [focusPhi, focusTheta] = focusRef.current
 
-        if (focusPhi > 0) {
-          const doublePi = Math.PI * 2
-          const distPositive = (focusPhi - current.phi + doublePi) % doublePi
-          const distNegative = (current.phi - focusPhi + doublePi) % doublePi
-          // Control the speed
-          if (distPositive < distNegative) {
-            current.phi += distPositive * 0.08
-          } else {
-            current.phi -= distNegative * 0.08
-          }
-          current.theta = current.theta * 0.92 + focusTheta * 0.08
-        }
+  //         if (focusPhi > 0) {
+  //           const doublePi = Math.PI * 2
+  //           const distPositive = (focusPhi - current.phi + doublePi) % doublePi
+  //           const distNegative = (current.phi - focusPhi + doublePi) % doublePi
+  //           // Control the speed
+  //           if (distPositive < distNegative) {
+  //             current.phi += distPositive * 0.08
+  //           } else {
+  //             current.phi -= distNegative * 0.08
+  //           }
+  //           current.theta = current.theta * 0.92 + focusTheta * 0.08
+  //         }
 
-        current.phi += 0.0025
+  //         current.phi += 0.0025
 
-        state.phi = current.phi
-        state.width = width * 2
-        state.height = width * 2
-      },
-    })
-    setTimeout(() => {
-      if (canvasRef.current) {
-        canvasRef.current.style.opacity = "1"
-      }
-    })
-    return () => {
-      globe.destroy()
-      window.removeEventListener("resize", onResize)
-    }
-  }, [])
+  //         state.phi = current.phi
+  //         state.width = width * 2
+  //         state.height = width * 2
+  //       },
+  //     })
+  //   }
+
+  //   setTimeout(() => {
+  //     if (canvasRef.current) {
+  //       canvasRef.current.style.opacity = "1"
+  //     }
+  //   })
+  //   return () => {
+  //     globe?.destroy()
+  //     window.removeEventListener("resize", onResize)
+  //   }
+  // }, [])
 
   const locationToAngles = (lat: number, long: number) => {
     return [
@@ -83,7 +88,7 @@ export default function Home() {
   }
 
   const resetFocus = () => {
-    focusRef.current = [0, 0.3]
+    focusRef.current = [0, 0]
   }
 
   const toggleFocus = () => {
@@ -96,8 +101,9 @@ export default function Home() {
 
   return (
     <>
-      <div className="absolute top-0 w-full">
-        <canvas
+      <div className="overflow-none absolute top-0 w-full">
+        {/* <Globe /> */}
+        {/* <canvas
           className="mx-auto"
           ref={canvasRef}
           style={{
@@ -106,7 +112,7 @@ export default function Home() {
             maxWidth: "100%",
             aspectRatio: 1,
           }}
-        />
+        /> */}
       </div>
       <LazyMotion features={domAnimation}>
         <div className="container relative mt-[10vh] flex justify-center">
