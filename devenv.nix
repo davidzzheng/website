e40b8e6a21117e063c9c101d8eb9abb68530ff38
David Zheng <hello@davidzheng.me>
2024-07-26 22:55:38 -0700

    initial commit

---
+++ devenv.nix
@@ -0,0 +1,30 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [ pkgs.git pkgs.bun ];
+
+  enterShell = ''
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+
+  # https://devenv.sh/services/
+  services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+  languages.typescript.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+  processes.web.exec = "bun run dev --port=3030";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
