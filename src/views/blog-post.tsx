29fc2b3988aa44ad2efc5fb2cf02ef3d052bab19
David Zheng <hello@davidzheng.me>
2024-07-31 15:52:46 -0700

    add reading time/word count

--- src/views/blog-post.tsx
+++ src/views/blog-post.tsx
@@ -1,16 +1,20 @@
 'use client'
+import Link from 'next/link'
 
-import { Breadcrumbs } from '@/components/breadcrumbs'
 import { Layout } from '@/components/layout'
 import { formatDate } from '@/lib/date'
-import Link from 'next/link'
 import { Post } from 'payload-types'
+
+const WPM = 150
 
 type BlogPostViewProps = {
   post: Post
 }
 
 export const BlogPostView = ({ post }: BlogPostViewProps) => {
+  const wordCount = post.contentHtml?.split(/\s+/).length ?? 0
+  const readingTime = Math.ceil(wordCount / WPM)
+
   return (
     <Layout>
       <Layout.Main className="rounded-lg bg-background/75 p-8 mb-16">
@@ -22,8 +26,11 @@ export const BlogPostView = ({ post }: BlogPostViewProps) => {
             <p className="text-muted-foreground ~text-lg/xl">{post.subtitle}</p>
           ) : null}
         </div>
-        <div className="flex items-center justify-between mb-8">
-          <p className="~text-xs/sm">Posted on {formatDate(post.createdAt)}</p>
+        <div className="flex flex-wrap gap-y-1 items-center justify-between mb-8 ~text-xs/sm">
+          <span>
+            {wordCount} words • {readingTime} min read
+          </span>
+          <p>Posted on {formatDate(post.createdAt)}</p>
         </div>
         <div
           className="prose mb-8 mt-4 text-lg text-foreground dark:prose-invert"
