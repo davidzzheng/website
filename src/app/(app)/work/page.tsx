a2dc70d6251ff08760dcbf218c05c83fe1e74cdd
David Zheng <hello@davidzheng.me>
2024-07-30 18:32:45 -0700

    use arr.reduce for node 20 compat

--- src/app/(app)/work/page.tsx
+++ src/app/(app)/work/page.tsx
@@ -1,17 +1,34 @@
 import { getPayload } from '@/lib/payload'
 import { WorkView } from '@/views/work'
+import { Work } from 'payload-types'
 
 export default async function WorkPage() {
-	const payload = await getPayload()
+  const payload = await getPayload()
 
-	const { docs: works } = await payload.find({
-		collection: 'work',
-	})
+  const { docs: works } = await payload.find({
+    collection: 'work',
+  })
 
-	const { fulltime: positions = [], project: projects = [] } = Object.groupBy(
-		works,
-		(work) => work.workType!,
-	)
+  // Object.groupBy is not supported on Vercel yet (needs Node 21)
+  // const { fulltime: positions = [], project: projects = [] } = Object.groupBy(
+  // 	works,
+  // 	(work) => work.workType!,
+  // )
 
-	return <WorkView positions={positions} projects={projects} />
+  const { fulltime: positions = [], project: projects = [] } = works.reduce<{
+    fulltime: Work[]
+    project: Work[]
+  }>(
+    (acc, work) => {
+      if (work.workType === 'fulltime') {
+        acc.fulltime.push(work)
+      } else if (work.workType === 'project') {
+        acc.project.push(work)
+      }
+      return acc
+    },
+    { fulltime: [], project: [] },
+  )
+
+  return <WorkView positions={positions} projects={projects} />
 }
