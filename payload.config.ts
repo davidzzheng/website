b9349e82d07ea3878e0cacab304b3c015a77ae91
David Zheng <hello@davidzheng.me>
2024-07-30 18:02:03 -0700

    cleanup for deployment

--- payload.config.ts
+++ payload.config.ts
@@ -2,12 +2,12 @@ import path from 'path'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { en } from 'payload/i18n/en'
 import {
-	BlocksFeature,
-	HTMLConverterFeature,
-	lexicalEditor,
-	lexicalHTML,
-	LinkFeature,
-	UploadFeature,
+  BlocksFeature,
+  HTMLConverterFeature,
+  lexicalEditor,
+  lexicalHTML,
+  LinkFeature,
+  UploadFeature,
 } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload'
 import sharp from 'sharp'
@@ -17,200 +17,200 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-	editor: lexicalEditor({
-		features: ({ defaultFeatures }) => [
-			...defaultFeatures,
-			LinkFeature({
-				fields: [
-					{
-						name: 'rel',
-						label: 'Rel Attribute',
-						type: 'select',
-						hasMany: true,
-						options: ['noopener', 'noreferrer', 'nofollow'],
-						admin: {
-							description:
-								'The rel attribute defines the relationship between a linked resource and the current document. This is a custom link field.',
-						},
-					},
-				],
-			}),
-			UploadFeature({
-				collections: {
-					uploads: {
-						fields: [
-							{
-								name: 'caption',
-								type: 'richText',
-								editor: lexicalEditor(),
-							},
-						],
-					},
-				},
-			}),
-			HTMLConverterFeature({}),
-		],
-	}),
-	collections: [
-		{
-			slug: 'users',
-			auth: true,
-			access: {
-				delete: () => false,
-				update: () => false,
-			},
-			fields: [],
-		},
-		{
-			slug: 'posts',
-			admin: {
-				useAsTitle: 'title',
-			},
-			fields: [
-				{
-					name: 'title',
-					type: 'text',
-				},
-				{
-					name: 'subtitle',
-					type: 'text',
-				},
-				{
-					name: 'content',
-					type: 'richText',
-				},
-				{
-					name: 'tags',
-					type: 'relationship',
-					relationTo: 'tags',
-					hasMany: true,
-				},
-				lexicalHTML('content', { name: 'contentHtml' }),
-			],
-		},
-		{
-			slug: 'tags',
-			fields: [
-				{
-					name: 'name',
-					type: 'text',
-				},
-				{ name: 'id', type: 'text', unique: true },
-			],
-			admin: {
-				useAsTitle: 'name',
-			},
-			labels: {
-				singular: 'Tag',
-				plural: 'Tags',
-			},
-		},
-		{
-			slug: 'media',
-			upload: true,
-			fields: [
-				{
-					name: 'text',
-					type: 'text',
-				},
-			],
-		},
-		{
-			slug: 'work',
-			fields: [
-				{
-					name: 'company',
-					type: 'text',
-				},
-				{
-					name: 'description',
-					type: 'richText',
-				},
-				{
-					name: 'startDate',
-					type: 'date',
-				},
-				{
-					name: 'endDate',
-					type: 'date',
-				},
-				{
-					name: 'workType',
-					label: 'Work Type',
-					type: 'select',
-					options: [
-						{ label: 'Full Time', value: 'fulltime' },
-						{ label: 'Project', value: 'project' },
-					],
-				},
-				{
-					name: 'technologies',
-					type: 'relationship',
-					relationTo: 'technologies',
-					hasMany: true,
-				},
-				lexicalHTML('description', { name: 'descriptionHtml' }),
-			],
-		},
-		{
-			slug: 'technologies',
-			admin: {
-				useAsTitle: 'name',
-			},
-			fields: [
-				{
-					name: 'name',
-					type: 'text',
-				},
-			],
-		},
-		{
-			slug: 'codeSnippets',
-			labels: {
-				singular: 'Code Snippet',
-				plural: 'Code Snippets',
-			},
-			fields: [
-				{
-					name: 'title',
-					type: 'text',
-					required: true,
-				},
-				{
-					name: 'code',
-					type: 'code',
-					required: true,
-				},
-			],
-		},
-	],
-	secret: process.env.PAYLOAD_SECRET ?? '',
-	typescript: {
-		outputFile: path.resolve(dirname, 'payload-types.ts'),
-	},
-	db: postgresAdapter({
-		pool: {
-			connectionString: process.env.POSTGRES_URL ?? '',
-		},
-	}),
-	i18n: {
-		supportedLanguages: { en },
-	},
-	onInit: async (payload) => {
-		const existingUsers = await payload.find({
-			collection: 'users',
-			limit: 1,
-		})
+  editor: lexicalEditor({
+    features: ({ defaultFeatures }) => [
+      ...defaultFeatures,
+      LinkFeature({
+        fields: [
+          {
+            name: 'rel',
+            label: 'Rel Attribute',
+            type: 'select',
+            hasMany: true,
+            options: ['noopener', 'noreferrer', 'nofollow'],
+            admin: {
+              description:
+                'The rel attribute defines the relationship between a linked resource and the current document. This is a custom link field.',
+            },
+          },
+        ],
+      }),
+      UploadFeature({
+        collections: {
+          uploads: {
+            fields: [
+              {
+                name: 'caption',
+                type: 'richText',
+                editor: lexicalEditor(),
+              },
+            ],
+          },
+        },
+      }),
+      HTMLConverterFeature({}),
+    ],
+  }),
+  collections: [
+    {
+      slug: 'users',
+      auth: true,
+      access: {
+        delete: () => false,
+        update: () => false,
+      },
+      fields: [],
+    },
+    {
+      slug: 'posts',
+      admin: {
+        useAsTitle: 'title',
+      },
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+        },
+        {
+          name: 'subtitle',
+          type: 'text',
+        },
+        {
+          name: 'content',
+          type: 'richText',
+        },
+        {
+          name: 'tags',
+          type: 'relationship',
+          relationTo: 'tags',
+          hasMany: true,
+        },
+        lexicalHTML('content', { name: 'contentHtml' }),
+      ],
+    },
+    {
+      slug: 'tags',
+      fields: [
+        {
+          name: 'name',
+          type: 'text',
+        },
+        { name: 'id', type: 'text', unique: true },
+      ],
+      admin: {
+        useAsTitle: 'name',
+      },
+      labels: {
+        singular: 'Tag',
+        plural: 'Tags',
+      },
+    },
+    {
+      slug: 'media',
+      upload: true,
+      fields: [
+        {
+          name: 'text',
+          type: 'text',
+        },
+      ],
+    },
+    {
+      slug: 'work',
+      fields: [
+        {
+          name: 'company',
+          type: 'text',
+        },
+        {
+          name: 'description',
+          type: 'richText',
+        },
+        {
+          name: 'startDate',
+          type: 'date',
+        },
+        {
+          name: 'endDate',
+          type: 'date',
+        },
+        {
+          name: 'workType',
+          label: 'Work Type',
+          type: 'select',
+          options: [
+            { label: 'Full Time', value: 'fulltime' },
+            { label: 'Project', value: 'project' },
+          ],
+        },
+        {
+          name: 'technologies',
+          type: 'relationship',
+          relationTo: 'technologies',
+          hasMany: true,
+        },
+        lexicalHTML('description', { name: 'descriptionHtml' }),
+      ],
+    },
+    {
+      slug: 'technologies',
+      admin: {
+        useAsTitle: 'name',
+      },
+      fields: [
+        {
+          name: 'name',
+          type: 'text',
+        },
+      ],
+    },
+    {
+      slug: 'codeSnippets',
+      labels: {
+        singular: 'Code Snippet',
+        plural: 'Code Snippets',
+      },
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'code',
+          type: 'code',
+          required: true,
+        },
+      ],
+    },
+  ],
+  secret: process.env.PAYLOAD_SECRET ?? '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.POSTGRES_URI ?? '',
+    },
+  }),
+  i18n: {
+    supportedLanguages: { en },
+  },
+  onInit: async (payload) => {
+    const existingUsers = await payload.find({
+      collection: 'users',
+      limit: 1,
+    })
 
-		if (existingUsers.docs.length === 0) {
-			await payload.create({
-				collection: 'users',
-				data: {
-					email: process.env.DEFAULT_EMAIL ?? '',
-					password: process.env.DEFAULT_PASSWORD,
-				},
-			})
-		}
-	},
-	sharp,
+    if (existingUsers.docs.length === 0) {
+      await payload.create({
+        collection: 'users',
+        data: {
+          email: process.env.DEFAULT_EMAIL ?? '',
+          password: process.env.DEFAULT_PASSWORD,
+        },
+      })
+    }
+  },
+  sharp,
 })
--- src/views/blog-post.tsx
+++ src/views/blog-post.tsx
@@ -7,44 +7,42 @@ import Link from 'next/link'
 import { Post } from 'payload-types'
 
 type BlogPostViewProps = {
-	post: Post;
-};
+  post: Post
+}
 
 export const BlogPostView = ({ post }: BlogPostViewProps) => {
-	return (
-		<Layout>
-			<Layout.Main className="rounded-lg bg-background/75 p-8">
-				<Breadcrumbs />
+  return (
+    <Layout>
+      <Layout.Main className="rounded-lg bg-background/75 p-8">
+        {/* <Breadcrumbs /> */}
 
-				<h1 className="my-3 font-bold tracking-tighter ~text-2xl/4xl">
-					{post.title}
-				</h1>
-				<div className="flex items-center justify-between">
-					<p className="~text-xs/sm">Posted on {formatDate(post.createdAt)}</p>
-				</div>
-				<div
-					className="prose mb-8 mt-4 text-lg text-foreground dark:prose-invert"
-					dangerouslySetInnerHTML={{
-						__html: post.contentHtml!,
-					}}
-				/>
-				<ul className="flex gap-2 text-muted-foreground transition hover:text-foreground">
-					{post.tags?.map((tag) => (
-						<Link
-							key={tag.name}
-							href={`/topics/${tag.id}`}
-							className="decorate-underline flex cursor-pointer"
-						>
-							<span className="text-xs">#</span> {tag.name}
-						</Link>
-					))}
-				</ul>
-			</Layout.Main>
-			{/* <Layout.Bottom className="rounded-lg bg-background/75 p-8"> */}
-			{/* 	<h1 className="mb-3 font-bold tracking-tighter ~text-lg/xl"> */}
-			{/* 		Other Posts */}
-			{/* 	</h1> */}
-			{/* </Layout.Bottom> */}
-		</Layout>
-	)
+        <h1 className="mb-3 font-bold tracking-tighter ~text-2xl/4xl">{post.title}</h1>
+        <div className="flex items-center justify-between">
+          <p className="~text-xs/sm">Posted on {formatDate(post.createdAt)}</p>
+        </div>
+        <div
+          className="prose mb-8 mt-4 text-lg text-foreground dark:prose-invert"
+          dangerouslySetInnerHTML={{
+            __html: post.contentHtml!,
+          }}
+        />
+        <ul className="flex gap-2 justify-end text-muted-foreground transition hover:text-foreground">
+          {post.tags?.map((tag) => (
+            <Link
+              key={tag.name}
+              href={`/topics/${tag.id}`}
+              className="decorate-underline flex cursor-pointer"
+            >
+              <span className="text-xs">#</span> {tag.name}
+            </Link>
+          ))}
+        </ul>
+      </Layout.Main>
+      {/* <Layout.Bottom className="rounded-lg bg-background/75 p-8"> */}
+      {/* 	<h1 className="mb-3 font-bold tracking-tighter ~text-lg/xl"> */}
+      {/* 		Other Posts */}
+      {/* 	</h1> */}
+      {/* </Layout.Bottom> */}
+    </Layout>
+  )
 }
