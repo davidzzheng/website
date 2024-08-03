6db8640abf6557b60cc91dd9aa38b0d59e263bc9
David Zheng <hello@davidzheng.me>
2024-08-03 15:35:29 -0700

    remove referrer from urls

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -1,1 +1,4 @@
 export const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
+
+export const removeRefParam = (url: string) =>
+  url.replace(/(\?|&)ref=[^&]+(&|$)/, '$1').replace(/\?$/, '')
