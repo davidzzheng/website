5d4e0f337a4e2a5b600c379f40a2a85aded44e3e
David Zheng <hello@davidzheng.me>
2024-08-04 14:34:48 -0700

    add heading deep linking

--- src/components/post-view/heading.tsx
+++ src/components/post-view/heading.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import { cn } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
+import { useCallback } from 'react'
 
 type HeadingProps = {
   heading: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
@@ -9,15 +13,24 @@ type HeadingProps = {
 
 export const Heading = ({ heading, id, children, className }: HeadingProps) => {
   const Comp = heading
+  const router = useRouter()
+
+  const handleClick = useCallback(async () => {
+    router.replace(`#${id}`, { scroll: false })
+
+    const url = window.location.href.replace(/#.*$/, `#${id}`)
+    await navigator.clipboard.writeText(url)
+  }, [id])
 
   return (
     <Comp
       id={id}
       className={cn(
-        'relative flex items-center cursor-pointer',
+        'relative flex items-center cursor-pointer hover:text-muted-foreground transition w-fit',
         "hover:before:content-['#'] before:absolute before:text-muted-foreground before:-ml-[21px] before:opacity-0 hover:before:opacity-100 before:transition",
         className,
       )}
+      onClick={handleClick}
     >
       {children}
     </Comp>
