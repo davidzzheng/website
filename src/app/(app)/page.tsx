811ab6924f8abf4b98e88a3f929c6e6947106f84
David Zheng <hello@davidzheng.me>
2024-08-04 00:57:08 -0700

    use default metadata instead

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { Metadata } from 'next'
 
 import { HomeView } from '@/views/home'
-
-export const metadata: Metadata = {
-  title: 'Home',
-}
 
 export default async function HomePage() {
   return <HomeView />
