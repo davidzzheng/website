a9c27977c43fffac65052cd20ed15c0f1440af74
David Zheng <hello@davidzheng.me>
2024-08-04 14:25:16 -0700

    add scroll to top to table of contents

--- src/components/scroll-to-top.tsx
+++ src/components/scroll-to-top.tsx
@@ -2,10 +2,14 @@
 
 import { ArrowUp } from 'lucide-react'
 
-import { Button } from './ui/button'
+import { Button, ButtonProps } from './ui/button'
 
-export const ScrollToTop = () => (
-  <Button variant="secondary" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+export const ScrollToTop = (props: ButtonProps) => (
+  <Button
+    variant="secondary"
+    {...props}
+    onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+  >
     <ArrowUp className="mr-2 size-4" />
     Back to top
   </Button>
