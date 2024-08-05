a85578bebdb592ffa47117bc4a3c98cafb2af50d
David Zheng <hello@davidzheng.me>
2024-08-05 00:17:07 -0700

    simplify works page

--- src/views/works.tsx
+++ src/views/works.tsx
@@ -17,86 +17,51 @@ export const WorkView = ({ positions, projects }: WorkViewProps) => {
   return (
     <Layout>
       <Layout.Main className="mb-16 rounded-lg bg-background/80 p-6">
-        <h1 className="~text-2xl/4xl mb-3 font-bold tracking-tighter">Work Experience</h1>
-        <section className="mb-12 flex flex-col gap-y-2">
-          <div className="flex flex-col gap-y-6">
-            {positions.map((position) => (
-              <div key={position.id} className="flex flex-col gap-y-2">
-                <div className="flex items-center justify-between">
-                  <a
-                    href={position.link!}
-                    className="~text-lg/2xl decorate-underline font-semibold"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    {position.company}
-                  </a>
-                  <p className="~text-xs/sm">
-                    {`${getDate(position.startDate!)} - ${getDate(position.endDate!)}`}
-                  </p>
-                </div>
-                <div
-                  className="prose dark:prose-invert max-w-full text-foreground"
-                  dangerouslySetInnerHTML={{
-                    __html: position.descriptionHtml!,
-                  }}
-                />
-                <ul className="flex flex-wrap gap-2">
-                  {position.technologies?.map((tag) => (
-                    <li
-                      key={(tag as Technology).name}
-                      className="cursor-default rounded bg-muted px-1 text-muted-foreground text-sm transition hover:text-foreground"
-                    >
-                      {(tag as Technology).name}
-                    </li>
-                  ))}
-                </ul>
+        {[
+          { header: 'Work Experience', items: positions },
+          { header: 'Projects', items: projects },
+        ].map(({ header, items }) => (
+          <div key={header}>
+            <h1 className="~text-2xl/4xl mb-3 font-bold tracking-tighter">{header}</h1>
+            <section className="mb-12 flex flex-col gap-y-2">
+              <div className="flex flex-col gap-y-8">
+                {items.map((position) => (
+                  <div key={position.id} className="flex flex-col gap-y-2">
+                    <div className="flex items-center justify-between">
+                      <a
+                        href={position.link!}
+                        className="~text-lg/2xl decorate-underline font-semibold"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        {position.company}
+                      </a>
+                      <p className="~text-xs/sm">
+                        {`${getDate(position.startDate!)} - ${getDate(position.endDate!)}`}
+                      </p>
+                    </div>
+                    <div
+                      className="prose dark:prose-invert mb-2 max-w-full text-foreground"
+                      dangerouslySetInnerHTML={{
+                        __html: position.descriptionHtml!,
+                      }}
+                    />
+                    <ul className="flex flex-wrap gap-2">
+                      {(position.technologies as Technology[])?.map(({ name }) => (
+                        <li
+                          key={name}
+                          className="cursor-default rounded bg-muted px-1 text-muted-foreground text-sm transition hover:text-foreground"
+                        >
+                          {name}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
               </div>
-            ))}
+            </section>
           </div>
-        </section>
-
-        <h1 className="~text-2xl/4xl mb-3 font-bold tracking-tighter">Projects</h1>
-        <section className="flex flex-col gap-y-2">
-          <div className="flex flex-col gap-y-6">
-            {projects.map((project) => (
-              <div key={project.id} className="flex flex-col gap-y-2">
-                <div className="flex items-center justify-between">
-                  <a
-                    href={project.link!}
-                    className="~text-lg/2xl decorate-underline font-semibold"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    {project.company}
-                  </a>
-
-                  <p className="~text-xs/sm">
-                    {`${getDate(project.startDate!)} - ${
-                      project.endDate ? getDate(project.endDate) : 'Present'
-                    }`}
-                  </p>
-                </div>
-                <div
-                  className="prose dark:prose-invert max-w-full text-foreground"
-                  dangerouslySetInnerHTML={{
-                    __html: project.descriptionHtml!,
-                  }}
-                />
-                <ul className="flex flex-wrap gap-2">
-                  {project.technologies?.map((tag) => (
-                    <li
-                      key={(tag as Technology).name}
-                      className="cursor-default rounded bg-muted px-1 text-muted-foreground text-sm transition hover:text-foreground"
-                    >
-                      {(tag as Technology).name}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-        </section>
+        ))}
       </Layout.Main>
     </Layout>
   )
