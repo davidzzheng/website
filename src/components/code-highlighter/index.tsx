a808f942d47f3a893b66ef57ef75d9cd2936dcc0
David Zheng <hello@davidzheng.me>
2024-08-05 20:36:28 -0700

    add styling for figure elements

--- src/components/code-highlighter/index.tsx
+++ src/components/code-highlighter/index.tsx
@@ -4,22 +4,25 @@ import { type CodeHighlighterOptions, codeToHtml, stripCodeDirectives } from '@/
 import { Clipboard } from '../ui/clipboard'
 
 import './index.scss'
+import { cn } from '@/lib/utils'
 
 type CodeHighlighterProps = {
   code: string
   lang?: CodeHighlighterOptions['lang']
   theme?: CodeHighlighterOptions['theme']
+  className?: string
 }
 
 export const CodeHighlighter = async ({
   code,
   lang = 'ts',
   theme = 'tokyo-night',
+  className,
 }: CodeHighlighterProps) => {
   const html = await codeToHtml(code, { lang, theme })
 
   return (
-    <div className="relative">
+    <div className={cn('relative', className)}>
       <Clipboard
         value={stripCodeDirectives(code)}
         className="absolute top-4 right-4 z-10 opacity-60 transition hover:opacity-100"
