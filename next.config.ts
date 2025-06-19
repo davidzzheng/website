a6f9559c3c06ac17ae93a733b3eaf380e31731ee
David Zheng <davidzzheng@users.noreply.github.com>
2025-06-19 11:46:28 -0700

    nvm

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import { withContentlayer } from 'next-contentlayer'
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-  output: 'export',
 } as NextConfig
 
 export default withContentlayer(nextConfig)
